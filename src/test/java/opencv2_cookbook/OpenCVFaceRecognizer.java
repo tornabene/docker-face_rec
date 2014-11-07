4627f2243d889fb7047bc44bbb57bc0c8dd51590
tindaro tornabene <tindaro.tornabene@ietservizi.it>
2014-11-07 16:28:19 +0100

    risolto problem del riconoscimento il sistema vuole 3 foto per volto

--- src/test/java/opencv2_cookbook/OpenCVFaceRecognizer.java
+++ src/test/java/opencv2_cookbook/OpenCVFaceRecognizer.java
@@ -1,16 +1,17 @@
 package opencv2_cookbook;
 
-import org.bytedeco.javacpp.opencv_contrib;
-
 import static org.bytedeco.javacpp.opencv_core.CV_32SC1;
 import static org.bytedeco.javacpp.opencv_highgui.CV_LOAD_IMAGE_GRAYSCALE;
 import static org.bytedeco.javacpp.opencv_highgui.imread;
-import ij.process.ImageProcessor;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.nio.DoubleBuffer;
 import java.nio.IntBuffer;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.bytedeco.javacpp.opencv_contrib;
 import org.bytedeco.javacpp.opencv_contrib.FaceRecognizer;
 import org.bytedeco.javacpp.opencv_core.Mat;
 import org.bytedeco.javacpp.opencv_core.MatVector;
@@ -18,13 +19,14 @@ import org.junit.Test;
 
 public class OpenCVFaceRecognizer {
 
+	
+	private FaceRecognizer faceRecognizer = opencv_contrib.createFisherFaceRecognizer();
+	
 	@Test
 	public void test1() {
 		try {
 
 			String trainingDir = "trainingDir";
-
-			
 			File root = new File(trainingDir);
 
 			FilenameFilter imgFilter = new FilenameFilter() {
@@ -38,33 +40,41 @@ public class OpenCVFaceRecognizer {
 			File[] imageFiles = root.listFiles(imgFilter);
 			System.out.println(imageFiles);
 
-			MatVector images = new MatVector(imageFiles.length);
+			
 			Mat labels = new Mat(imageFiles.length, 1, CV_32SC1);
 			IntBuffer labelsBuf = labels.getIntBuffer();
+			
 			int counter = 0;
-
+			Map<Integer,String> tableImages = new HashMap<Integer, String>();
+			MatVector images = new MatVector(imageFiles.length);
+			
 			for (File image : imageFiles) {
-				Mat img = imread(image.getAbsolutePath(),
+				String filename = image.getAbsolutePath();
+				Mat img = imread(filename,
 						CV_LOAD_IMAGE_GRAYSCALE);
 
+				Integer label = counter++;
+				images.put(label, img);
 				
-				int label = Integer.parseInt(image.getName().split("\\-")[0]);
-				images.put(counter, img);
-				labelsBuf.put(counter, label);
-				counter++;
-				 
+				tableImages.put(label, filename);
+				
+				labelsBuf.put(label,label);
 			}
 
+		 	faceRecognizer.train(images, labels);
 
-			FaceRecognizer faceRecognizer = opencv_contrib.createFisherFaceRecognizer();
-			faceRecognizer.train(images, labels);
+		 	System.out.println(tableImages);
+			String fileImageTest = "/home/tino/git/tornabene/docker-face_rec/testImage/peppe.jpg";
+			//String fileImageTest = "/home/tino/git/tornabene/docker-face_rec/trainingDir/1-peppe.jpg";
 			
-			String fileImageTest = "/home/tino/git/tornabene/docker-face_rec/testImage/IMG_20141107_123939.jpg";
 			Mat testImage = imread(fileImageTest, CV_LOAD_IMAGE_GRAYSCALE);
 			
-			System.out.println(testImage);
-			int predictedLabel = faceRecognizer.predict(testImage);
-			System.out.println("Predicted label: " + predictedLabel);
+			DoubleBuffer confidence = DoubleBuffer.allocate(2);
+			
+			int predictedLabel = faceRecognizer.predict(testImage);//, labelsBuf, confidence);
+			System.out.println("Predicted label:; " + predictedLabel);
+			Mat imageTrovata = images.get(predictedLabel);
+			System.out.println("Predicted imageTrovata: " + imageTrovata );
 			
 		} catch (UnsatisfiedLinkError e) {
 			e.printStackTrace();
